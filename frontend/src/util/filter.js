982f380088b92af0943d8d53316d2a02f8446876
heli576 <helivakharia@gmail.com>
2021-03-14 01:29:29 +0530

    Added org dashboard functionalities

--- frontend/src/util/filter.js
+++
@@ -1,9 +0,0 @@
-export const getTrials=(keyword)=>{
-  return fetch(`/search?keyword=${keyword}`,{
-    method:"GET"
-  })
-  .then(response=>{
-    return response.json();
-  })
-  .catch(err=>console.log(err));
-}
