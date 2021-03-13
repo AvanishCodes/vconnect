d2212ab6da1d41039ad506bc165ac94c835ea8cc
Aditya Paul <aditya.paul18@gmail.com>
2021-03-13 23:52:49 +0530

    org trial edit added

---
+++ frontend/src/pages/Trialedit/Trialedit.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar';
+import { Avatar, Button } from '@material-ui/core';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+
+function Trialedit() {
+    return (
+        <div>
+            <Navbar/>
+            <div className="org_header">
+                    <Avatar className="orgavatar" src="https://cdn.corporatefinanceinstitute.com/assets/types-of-organizations1.jpeg" alt="org" />
+                    <h1>ABC Organization</h1>
+                    <span>Clinical trials</span>
+                </div>
+                <div className="track" >
+                    ABC Organization <ArrowForwardIosIcon /> <span>Your Trial</span> <ArrowForwardIosIcon /> <span>Edit Trial</span> 
+                </div>
+                <div className="lower_home">
+                </div>
+                <Button className="savebutton" variant="contained" >save changes</Button>
+        </div>
+    )
+}
+
+export default Trialedit
