ac89c670446fa51058d365271a5931fb1ca8c54e
Avanish Gupta <ui19co15@iiitsurat.ac.in>
2021-03-13 23:57:11 +0530

    Fixed login and signup page view, changes in readme

--- README.md
+++ README.md
@@ -6,7 +6,15 @@ The repository for HackBash
 
 >> Every year many people die because of the disease for which there is no cure available, and the reason is, we cannot infect new people to get a cure. We seek to help all of them by giving them a chance to help the medical studies by participating in clinical trials and contribute their bit for the better of humanity. This platform does not allow the information of the users to be exposed to the clinical trial organizations till before they actually needto contact them for physical confirmation.
 
-- ### The users have the functionality to 
-- > Check the current clinical trials going around based on <b>Age</b>, <b>Gender</b>, <b>City</b>, <b>State</b> or country.
-- > submit primary application to the organization to participate in the trial without submitting the name, email, any social profile or address.
-- > Accept or reject the invitation of an organization after primary screenign
+### The users have the functionality to 
+> - Check the current clinical trials going around based on <b>Age</b>, <b>Gender</b>, <b>City</b>, <b>State</b> or country.
+> - submit primary application to the organization to participate in the trial without submitting the name, email, any social profile or address.
+> - Accept or reject the invitation of an organization after primary screenign
+
+
+
+### Contributions:
+- [<b>Aditya Paul</b>](https://www.github.com/adityapaul18)        -> Front end major
+- [<b>Avanish Gupta</b>](https://www.github.com/AvanishCodes)      -> Ideation
+- [<b>Heli Vakharia</b>](https://www.github.com/heli576)      -> Back end major
+- [<b>Nishant Singh</b>](https://www.github.com/iamoo7)      -> Frontend Design
--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import { Button } from '@material-ui/core';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import './Login.css';
 
 function Login() {
-    
+
     const [mail, setmail] = useState("")
     const [password, setpassword] = useState("")
-   
+
     const history = useHistory();
     const handlelogin = (e) => {
         e.preventDefault();
-        console.log(mail , password);
+        console.log(mail, password);
         setmail("")
         setpassword("")
     }
@@ -22,18 +22,19 @@ function Login() {
             </div>
 
             <div className="login_right">
-            <div className="login_header"><h1>Log <span>in</span></h1></div>
-            <div className="upper_container">
-                <form className="login_form">
-                    Email
-                    <input className="login_input"value={mail} placeholder="Your Email" type="email" name="email" onChange={(e) => setmail(e.target.value)} />
+                <div className="login_header"><h1>Log <span>in</span></h1></div>
+                <div className="upper_container">
+                    <form className="login_form">
+                        Email
+                    <input className="login_input" value={mail} placeholder="Your Email" type="email" name="email" onChange={(e) => setmail(e.target.value)} />
                     Password
-                    <input className="login_input"value={password} placeholder="Your Password" type="password" name="password" onChange={(e) => setpassword(e.target.value)}/>
-                </form>
+                    <input className="login_input" value={password} placeholder="Your Password" type="password" name="password" onChange={(e) => setpassword(e.target.value)} />
+                    </form>
                     <p >forgot password</p>
-                    <Button className="login_button" variant="contained" onClick={handlelogin} ><ArrowForwardIcon/></Button>
-            </div>
-            <div className="login-lower" >dont have an accout ?  <span onClick={() => history.push('./signup') } >Sign up</span></div>
+                    <Button className="login_button" variant="contained" onClick={handlelogin} ><ArrowForwardIcon /></Button>
+                </div>
+                <div className="login-lower" >dont have an accout ?  <span onClick={() => history.push('./signup')} >Sign up</span></div>
+                <div className="login-lower" >I'm here just to roam around!<span onClick={() => history.push('./')}>Take me home</span></div>
             </div>
         </div>
     )
