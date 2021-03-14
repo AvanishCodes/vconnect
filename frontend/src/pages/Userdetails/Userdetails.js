cdc2580009a21230e337d59568b0ebad36b47b73
Aditya Paul <aditya.paul18@gmail.com>
2021-03-14 09:04:07 +0530

    all page ui done

--- frontend/src/pages/Userdetails/Userdetails.js
+++ frontend/src/pages/Userdetails/Userdetails.js
@@ -15,10 +15,54 @@ function Userdetails() {
             <div className="track" >
                 User <ArrowForwardIosIcon /> <span>Account</span>
             </div>
-            <div className="lower_home">
+            <div className="field">
+                <table className="checktable">
+                    <tr className="rowt" >
+                        <td>Name of user :</td>
+                        <td className="field" >Name</td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>Address Line 1:</td>
+                        <td className="field" >Address Line </td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>City:</td>
+                        <td className="field" >City </td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>State:</td>
+                        <td className="field" >State</td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>Country:</td>
+                        <td className="field" >Country</td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>Pin Code:</td>
+                        <td className="field" >Pin Code</td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>Website:</td>
+                        <td className="field" >Website </td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>Help-line Number: </td>
+                        <td className="field" >phone num</td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>Gender: </td>
+                        <td className="field" > Gender  </td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>Age: </td>
+                        <td className="field" >Age</td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>Keyword: </td>
+                        <td className="field" >Keyword</td>
+                    </tr>
+                </table>
             </div>
-            <Button className="savebutton" variant="contained" >save changes</Button>
-            {/* <div className="footer"><Footer /></div> */}
             <Footer />
         </div>
     )
