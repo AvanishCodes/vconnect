fcd1789d1f4a52151d444f91a5b2754bc91d926c
Aditya Paul <aditya.paul18@gmail.com>
2021-03-14 09:00:20 +0530

    checkapp page ui done

--- frontend/src/pages/CheckApp/CheckApp.js
+++ frontend/src/pages/CheckApp/CheckApp.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import Navbar from '../../components/Navbar/Navbar';
-import { Avatar } from '@material-ui/core';
+import { Avatar, Button } from '@material-ui/core';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import Footer from '../../components/Footer/Footer';
-
+import './CheckApp.css'
 function CheckApp() {
+    const [keyword,setKeyword] = useState("")
     return (
         <div>
             <Navbar />
@@ -16,10 +17,54 @@ function CheckApp() {
             <div className="track" >
                 User <ArrowForwardIosIcon /> <span>Your Trials</span> <ArrowForwardIosIcon /> <span>check status</span>
             </div>
-            <div className="lower_home">
+            <div className="field">
+                <table className="checktable">
+                    <tr className="rowt" >
+                        <td>Name of user :</td>
+                        <td className="field" >Name</td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>Address Line 1:</td>
+                        <td className="field" >Address Line </td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>City:</td>
+                        <td className="field" >City </td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>State:</td>
+                        <td className="field" >State</td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>Country:</td>
+                        <td className="field" >Country</td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>Pin Code:</td>
+                        <td className="field" >Pin Code</td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>Website:</td>
+                        <td className="field" >Website </td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>Help-line Number: </td>
+                        <td className="field" >phone num</td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>Gender: </td>
+                        <td className="field" > Gender  </td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>Age: </td>
+                        <td className="field" >Age</td>
+                    </tr>
+                    <tr className="rowt" >
+                        <td>Keyword: </td>
+                        <td className="field" >Keyword</td>
+                    </tr>
+                </table>
             </div>
-            {/* <div className="footer"><Footer /></div> */}
-            {/* { } */}
             <Footer />
         </div>
     )
